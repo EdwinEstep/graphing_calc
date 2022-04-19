7dcc93995b50735f3b27d0a3d08d7a8283e33e34
ece0004 <ece0004@uah.edu>
2022-04-19 10:24:52 -0500

    Add separate dir for verif

---
+++ verif/testbenches/input_test.sv
@@ -0,0 +1,19 @@
+module input_test();
+    logic clk;
+
+    always(@posedge clk)
+        #10 clock <= ~clk;
+
+
+
+    cmd_parser uut(
+        .clk(clk),
+        .srst(srst),
+        .rx_dv(rx_dv),
+        .rx_byte(byte),
+        .dec_out(dec_out),
+        .stack_wr_data(wr_data),
+        .stack_wr_en(wr_en),
+        .opcode(opcode)
+    );
+endmodule
