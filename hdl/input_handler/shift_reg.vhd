656d7d3b15011ef179c983fcc4d7caf54ffc5a31
EdwinEstep <edwin.estep@gmail.com>
2022-04-19 08:07:12 -0500

    Add variable-width/length shift register for temp input storage

--- hdl/input_handler/shift_reg.vhd
+++ hdl/input_handler/shift_reg.vhd
@@ -1,12 +1,18 @@
 -- clearable shift-reg for storing variable-width values
 
-library ieee;
-use ieee.std_logic_1164.ALL;
-
-
 
 library ieee;
+use ieee.std_logic_1164.all;
+
+package sreg_types is
+    -- declare index range and width later
+    type vec_array is array(natural range <>) of std_logic_vector;
+end package sreg_types;
+
+
+library ieee;
 use ieee.std_logic_1164.ALL;
+use work.sreg_types.all;
 
 
 entity shift_reg is
@@ -17,24 +23,48 @@ entity shift_reg is
   port (
         clk      : in std_logic;
         srst     : in std_logic;
+        ctrl    : in std_logic_vector(1 downto 0);
         
         shift_in : in std_logic_vector(WIDTH-1 downto 0);
-        q_out    : out array(0 to LENGTH-1) of std_logic_vector(WIDTH-1 downto 0)
+
+        rdy      : out std_logic;
+        q_out    : out vec_array(0 to LENGTH-1)(WIDTH-1 downto 0)
     );
 end shift_reg;
 
 
-
 architecture behavioral of shift_reg is
+    signal i_reg : vec_array(0 to LENGTH-1)(WIDTH-1 downto 0);
+    signal i_cntr : integer range 0 to LENGTH-1;
 begin
     p1 : process(clk)
     begin
-        if(clk'event and clk='1') then  
-        if(srst='1') then 
-            -- LE RESET
-        else
-            
-        end if;
+        if(clk'event and clk='1') then 
+            if(srst='1') then 
+                -- LE RESET
+                i_cntr <= 0;
+            else
+                case ctrl is
+                    when "00" => -- CLEAR
+                        i_cntr <= 0;
+                    when "01" => -- SHIFT IN
+                        for i in 1 to LENGTH-1 loop
+                            i_reg(i) <= i_reg(i-1);
+                        end loop;
+                        i_reg(0) <= shift_in;
+                        i_cntr <= i_cntr + 1;
+                    when "10" => -- SHIFT BACK BY 1
+                        for i in LENGTH-2 downto 0 loop
+                            i_reg(i) <= i_reg(i-1);
+                        end loop;
+                        i_reg(LENGTH-1) <= (others => '0');
+                        i_cntr <= i_cntr - 1;
+                    when others =>
+                        i_reg <= i_reg;
+                end case;
+            end if;
         end if;
     end process;
+
+    q_out <= i_reg;
 end behavioral;
--- sim/testbenches/input_test.sv
+++ sim/testbenches/input_test.sv
@@ -7,6 +7,13 @@ module input_test();
 
 
     cmd_parser uut(
-
+        .clk(clk),
+        .srst(srst),
+        .rx_dv(rx_dv),
+        .rx_byte(byte),
+        .dec_out(dec_out),
+        .stack_wr_data(wr_data),
+        .stack_wr_en(wr_en),
+        .opcode(opcode)
     );
 endmodule
